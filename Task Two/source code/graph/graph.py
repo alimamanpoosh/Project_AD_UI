e0fc353f4de66fb8ce1a29adf08cebd441ef9ed0
pouria <pouria.sameti80@gmail.com>
2022-06-27 02:16:13 +0430

    task2:the get_shortest_path method from WeightedGraph class by using Dijkstra's algorithm was implemented

--- Task Two/source code/graph/graph.py
+++ Task Two/source code/graph/graph.py
@@ -17,10 +17,32 @@ class Path:
         return f"{self.__nodes}"
 
 
+class Node:
+    def __init__(self, label: str):
+        self.__label = label
+        self.__edges = []
+
+    @property
+    def edges(self):
+        return self.__edges
+
+    @property
+    def label(self):
+        return self.__label
+
+    def add_edge(self, target, weight: int):
+        edge = Edge(self, target, weight)
+        self.edges.append(edge)
+
+    def __str__(self):
+        return f"{self.__label}"
+
+
 class Edge:
-    def __init__(self, from_node: None, to_node: str, weight: int):
-        self.__from_node = from_node
-        self.__to_node = to_node
+
+    def __init__(self, from_node: Node, to_node: Node, weight: int):
+        self.__from_node: Node = from_node
+        self.__to_node: Node = to_node
         self.__weight = weight
 
     @property
@@ -36,23 +58,7 @@ class Edge:
         return self.__weight
 
     def __str__(self):
-        return f"{self.__from_node} -> {self.__to_node}"
-
-
-class Node:
-    def __init__(self, label: str):
-        self.__label = label
-        self.__edges = []
-
-    @property
-    def edges(self):
-        return self.__edges
-
-    def add_edge(self, target, weight: int):
-        self.__edges.append(Edge(self, target, weight))
-
-    def __str__(self):
-        return self.__label
+        return f"{str(self.__from_node)} -> {str(self.__to_node)}"
 
 
 class WeightedGraph:
@@ -68,12 +74,72 @@ class WeightedGraph:
         self.nodes[label] = Node(label)
 
     def add_edge(self, from_node: str, to_node: str, weight: int):
+        origin: Node = self.nodes.get(from_node)
+        target: Node = self.nodes.get(to_node)
+
+        if not origin or not target:
+            raise Exception("There is not Node for creating Edge")
+
+        origin.add_edge(target, weight)
+        target.add_edge(origin, weight)
+
+    def get_shortest_path(self, from_node, to_node):
         from_node: Node = self.nodes.get(from_node)
         to_node: Node = self.nodes.get(to_node)
 
         if not from_node or not to_node:
-            raise Exception("There is not Node for creating Edge")
+            raise Exception("There is not Node for finding the shortest path")
 
-        from_node.add_edge(to_node, weight)
-        to_node.add_edge(from_node, weight)
+        distances = dict()  # key: Node  value:  Integer
 
+        for node in self.nodes.values():
+            distances[node] = sys.maxsize
+
+        distances[from_node] = 0
+
+        visited = set()
+
+        queue = PriorityQueue()
+        queue.put((0, from_node))
+
+        previous = dict()  # key: Node  value: Node
+
+        while not queue.empty():
+
+            current: Node = queue.get()[1]
+            visited.add(current)
+
+            for edge in current.edges:
+                if edge.to_node in visited:
+                    continue
+
+                new_distance = distances.get(current) + edge.weight
+
+                if new_distance < distances.get(edge.to_node):
+                    distances[edge.to_node] = new_distance
+                    previous[edge.to_node] = current
+                    queue.put((new_distance, edge.to_node))
+
+        return WeightedGraph.__build_path(previous, to_node)
+
+    @classmethod
+    def __build_path(cls, previous: dict, to_node: Node) -> Path:
+        stack = [to_node]
+        previous_node = previous.get(to_node)
+
+        while previous_node:
+            stack.append(previous_node)
+            previous_node: Node = previous.get(previous_node)
+
+        p = Path()
+        while stack:
+            p.add_node(stack.pop().label)
+
+        return p
+
+    def print_graph(self):
+        for node in self.nodes.values():
+            s = ""
+            for e in node.edges:
+                s = s + str(e.to_node) + ","
+            print(node, " is connected to ", s)
